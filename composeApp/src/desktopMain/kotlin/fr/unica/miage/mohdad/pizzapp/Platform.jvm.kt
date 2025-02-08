5bcc4f4b74025db84b45a195b8041eb08e848285
Ralph-Mohdad <104560729+Ralph-Modad@users.noreply.github.com>
2025-02-08 14:08:23 +0100

    multiplateforme

---
+++ composeApp/src/desktopMain/kotlin/fr/unica/miage/mohdad/pizzapp/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package fr.unica.miage.mohdad.pizzapp
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
